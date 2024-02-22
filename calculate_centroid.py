8f59827238cf6c5cb4863a74269d69d70331690a
biribirii <52583917+biribirii@users.noreply.github.com>
2024-02-21 21:42:51 -0800

    black reforamt

--- calculate_centroid.py
+++ calculate_centroid.py
@@ -8,32 +8,39 @@ import geopandas as gpd
 
 import argparse
 
+
 def full_address(df):
-    if 'ZIP' in df.columns:
-        zip_col = 'ZIP'
+    if "ZIP" in df.columns:
+        zip_col = "ZIP"
     else:
-        zip_col = 'ZIPCODE'
+        zip_col = "ZIPCODE"
 
-    if 'STATE' in df.columns:
-        state_col = 'STATE'
+    if "STATE" in df.columns:
+        state_col = "STATE"
     else:
-        state_col = 'STNAME'
+        state_col = "STNAME"
 
-    if 'ADDRESS' in df.columns:
-        addr_col = 'ADDRESS'
-    elif 'ADDRESS' not in df.columns:
-        addr_col = 'STREET'
-    elif ('STREET' not in df.columns) and ('ADDRESS' not in df.columns):
-        addr_col = 'STD_ADDR_B'
+    if "ADDRESS" in df.columns:
+        addr_col = "ADDRESS"
+    elif "ADDRESS" not in df.columns:
+        addr_col = "STREET"
+    elif ("STREET" not in df.columns) and ("ADDRESS" not in df.columns):
+        addr_col = "STD_ADDR_B"
 
-    df['Full_Address'] = df[[addr_col, 'CITY', state_col]].fillna('NaN').agg(', '.join, axis=1) + ' ' + df[zip_col].astype('str')
+    df["Full_Address"] = (
+        df[[addr_col, "CITY", state_col]].fillna("NaN").agg(", ".join, axis=1)
+        + " "
+        + df[zip_col].astype("str")
+    )
 
     return df
+
 
 def read_shp(file, rows=100):
     df = gpd.read_file(file, rows=rows)
 
     return df
+
 
 def convert_EPSG4326(dict):
     """
@@ -45,27 +52,31 @@ def convert_EPSG4326(dict):
 
     return dict
 
+
 def get_centroid(dict):
     """
     Returns Point object or a coordinate tuple (x, y)
     """
     for fname in dict:
-        if 'x' in dict[fname].columns:
-            dict[fname] = dict[fname].rename(columns={'x': 'source_lon', 'y': 'source_lat'})
-            dict[fname]['Place_type'] = os.path.basename(fname)
+        if "x" in dict[fname].columns:
+            dict[fname] = dict[fname].rename(
+                columns={"x": "source_lon", "y": "source_lat"}
+            )
+            dict[fname]["Place_type"] = os.path.basename(fname)
         else:
-            dict[fname]['source_centroid'] = dict[fname]['geometry'].centroid
-            dict[fname]['source_lon'] = dict[fname]['geometry'].centroid.x
-            dict[fname]['source_lat'] = dict[fname]['geometry'].centroid.y
-            dict[fname]['Place_type'] = os.path.basename(fname)
+            dict[fname]["source_centroid"] = dict[fname]["geometry"].centroid
+            dict[fname]["source_lon"] = dict[fname]["geometry"].centroid.x
+            dict[fname]["source_lat"] = dict[fname]["geometry"].centroid.y
+            dict[fname]["Place_type"] = os.path.basename(fname)
 
     return dict
+
 
 def keep_columns(dict):
     """
     Keep full address string and calculated centroid (lon/lat)
     """
-    cols = ['Full_Address', 'Place_type', 'source_centroid', 'source_lon', 'source_lat']
+    cols = ["Full_Address", "Place_type", "source_centroid", "source_lon", "source_lat"]
     new_dict = {}
     for fname in dict:
         print(fname)
@@ -73,40 +84,51 @@ def keep_columns(dict):
 
     return new_dict
 
+
 def save_shp(dict, save_dir):
     for fname in dict:
         # dict[fname]['source_centroid'] = gpd.GeoSeries.from_wkt(dict[fname]['source_centroid'])
-        shp_file = dict[fname].set_geometry('source_centroid')
+        shp_file = dict[fname].set_geometry("source_centroid")
         # shp_file = gpd.GeoDataFrame(dict[fname], geometry=dict[fname]['source_centroid'])
         # shp_file.to_file(os.path.join(save_dir, ('{}.shp'.format(fname))), driver='ESRI Shapefile')
         save_path = os.path.join(save_dir, f"{fname}")
         create_dir(save_path)
-        shp_file.to_file(save_path, driver='ESRI Shapefile')
+        shp_file.to_file(save_path, driver="ESRI Shapefile")
+
 
 def create_dir(save_dir):
     if not os.path.exists(save_dir):
         os.makedirs(save_dir)
 
+
 def main():
-    parser = argparse.ArgumentParser(description='Calculate centroids from geoshape files')
+    parser = argparse.ArgumentParser(
+        description="Calculate centroids from geoshape files"
+    )
 
     # args
-    parser.add_argument('data_dir', required=True, help='path of geoshape files')
-    parser.add_argument('output_dir', required=True, help='path to save centroid geoshape files')
-    parser.add_argument('rows', default=100, help='Number of rows to count per geoshape file')
+    parser.add_argument("data_dir", required=True, help="path of geoshape files")
+    parser.add_argument(
+        "output_dir", required=True, help="path to save centroid geoshape files"
+    )
+    parser.add_argument(
+        "rows", default=100, help="Number of rows to count per geoshape file"
+    )
 
     args = parser.parse_args()
 
     HIFLD_path = args.data_dir
-    shp_files = [os.path.join(root, name) \
-                 for root, dirs, files in os.walk(HIFLD_path) \
-                 for name in files \
-                 if name.endswith(('.shp'))]
+    shp_files = [
+        os.path.join(root, name)
+        for root, dirs, files in os.walk(HIFLD_path)
+        for name in files
+        if name.endswith((".shp"))
+    ]
 
     dict_address = dict()
     for file in shp_files:
-        basename = os.path.basename(file).split('/')[0]
-        fname = os.path.basename(basename).split('.')[0]
+        basename = os.path.basename(file).split("/")[0]
+        fname = os.path.basename(basename).split(".")[0]
 
         # print(file)
         df = read_shp(file, rows=args.rows)
@@ -122,5 +144,6 @@ def main():
 
     save_shp(final_dict, save_dir)
 
+
 if __name__ == "__main__":
-    print('Geoshape files with centroids saved')
+    print("Geoshape files with centroids saved")
